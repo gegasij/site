016f2040ea67165bb6e42c4c77dd8d9dc92d09f9
prost <vlad.reut@outlook.com>
2023-01-30 15:23:14 +0100

    added site page

--- site.js
+++ site.js
@@ -1,14 +1,10 @@
-const http = require('http');
+const express = require("express");
+const app = express();
 
-const hostname = '0.0.0.0';
-const port = 4242;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
